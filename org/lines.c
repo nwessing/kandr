d64221b0a169e8db4f3bb17a7c9b5f35e17e294b
Nick Wessing <nwessing3@gmail.com>
2018-03-08 07:05:11 -0600

    add function pointer stuff

---
+++ org/lines.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLEN 1000
+
+int getline_(char[], int);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = getline_(line, MAXLEN)) > 0) {
+        if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+            return -1;
+        } else {
+            line[len-1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    }
+
+    return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0) {
+        printf("%s\n", *lineptr++);
+    }
+}
+
+int getline_(char s[], int lim)
+{
+    int c, i;
+    
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
---
+++ sort_fp.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXLINES 5000
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort_(void *lineptr[], int left, int right,
+            int (*comp)(void *, void *));
+int numcmp(char *, char *);
+
+int main(int argc, char *argv[])
+{
+    int nlines;
+    int numeric = 0;
+
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) {
+        numeric = 1;
+    }
+
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        writelines(lineptr, nlines);
+        qsort_((void **) lineptr, 0, nlines - 1, 
+            (int (*)(void *, void *))(numeric ? numcmp : strcmp));
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("input too big to sort\n");
+        return 1;
+    }
+}
+
+void qsort_(void *v[], int left, int right, int (*comp)(void *, void *))
+{
+    int i, last;
+    void swap(void *v[], int, int);
+
+    if (left >= right) {
+        return;
+    }
+
+    swap(v, left, (left+right)/2);
+    last = left;
+    for (i = left + 1; i <= right; i++) {
+        if((*comp)(v[i], v[left]) < 0) {
+            swap(v, ++last, i);
+        }
+    }
+
+    swap(v, left, last);
+    qsort_(v, left, last - 1, comp);
+    qsort_(v, last+1, right, comp);
+}
+
+int numcmp(char *s1, char *s2) 
+{
+    double v1, v2;
+
+    v1 = atof(s1);
+    v2 = atof(s2);
+
+    if (v1 < v2) {
+        return -1;
+    } else if (v1 > v2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void swap(void *v[], int i, int j)
+{
+    void *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
